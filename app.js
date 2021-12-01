1fad5f645f5f64a13f2baa3a4df7c3d2ac2f93cd
MarcosRossoni <marcosrossoni9@gmail.com>
2021-12-01 00:34:26 -0300

    excuida uma linha do app.js

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ function calcular() {
 
     
     let table = document.createElement('table');
-    //let thead = document.createElement('thead');
     let tbody = document.createElement('tbody');
 
     //table.appendChild(thead);
